ee33ed1c78f21789667b1a4a00fb59e17cb5221e
ibrahim378Edx <werdewer@yahoo.com>
2023-09-07 16:13:01 +0300

    task 11

---
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def pow(a, b):
+    z = 1
+    for i in range(0, abs(b)):
+            if b > 0:
+                z *= a
+            else:
+                 z /= a
+    return z
+
