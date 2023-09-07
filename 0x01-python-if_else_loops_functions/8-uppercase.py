ff3fd49fd33b9fe50d9506a8d958cf10084c5618
ibrahim378Edx <werdewer@yahoo.com>
2023-09-07 14:44:39 +0300

    task 8

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,8 @@
+#!/usr/bin/bash
+def uppercase(str):
+    for i in range(0, len(str)):
+        z = ord(str[i])
+        if (z >= 97 and z <= 122):
+            z = z - 32
+            str = str.replace(str[i], chr(z))
+    print("{}".format(str))
