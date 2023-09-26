ea1c533a398cc6421b1481f011701e3d8722ead8
ibrahim378Edx <werdewer@yahoo.com>
2023-09-26 15:05:39 +0300

    task 1

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
     try:
-        x = int(value)
-        print("{:d}".format(x))
-    except ValueError:
+        x = value / 10
+        print("{:d}".format(value))
+    except (ValueError, TypeError):
         return False
     else:
         return True
