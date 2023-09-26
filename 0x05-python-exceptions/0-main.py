d8e200173172990fb565e1e62e373a3a7c2125f7
ibrahim378Edx <werdewer@yahoo.com>
2023-09-27 01:33:15 +0300

    task 0

--- 0x05-python-exceptions/0-main.py
+++ 0x05-python-exceptions/0-main.py
@@ -3,7 +3,8 @@ safe_print_list = __import__('0-safe_print_list').safe_print_list
 
 my_list = [1, 2, 3, 4, 5]
 mylist = [1, 2, 3, 4]
-
+newlist = []
+z = [1,2,3,4]
 nb_print = safe_print_list(my_list, 2)
 print("nb_print: {:d}".format(nb_print))
 nb_print = safe_print_list(my_list, len(my_list))
@@ -12,3 +13,8 @@ nb_print = safe_print_list(my_list, len(my_list) + 2)
 print("nb_print: {:d}".format(nb_print))
 nb_print = safe_print_list(mylist, len(mylist)+10)
 print("nb_print: {:d}".format(nb_print))
+nb_print = safe_print_list(newlist, 0)
+print("nb_print: {:d}".format(nb_print))
+nb_print = safe_print_list(z, 0)
+print("nb_print: {:d}".format(nb_print))
+
--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -2,6 +2,9 @@
 def safe_print_list(my_list=[], x=0):
     if  my_list == []:
         return 0
+    if x == 0:
+        print("{}".format(my_list[0]))
+        return 1
     for i in range(0, x):
         try:
             print("{}".format(my_list[i]), end="")
