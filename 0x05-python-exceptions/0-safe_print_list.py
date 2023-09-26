d10edffe3168960c14e6b41a78c058817edce2f8
ibrahim378Edx <werdewer@yahoo.com>
2023-09-27 01:19:40 +0300

    task 0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    if len(my_list) == 0:
-        print("")
+    if not bool(my_list) == 0:
         return 0
     for i in range(0, x):
         try:
